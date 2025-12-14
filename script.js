9690df1d0c2e8168322f495a6235a7b9dff372e7
Javohir <javohirrahimjonov546@gmail.com>
2025-12-14 16:31:57 +0300

    Middle built

---
+++ script.js
@@ -0,0 +1,55 @@
+import {
+  elHands,
+  elUser,
+  elRobot,
+  elBtn,
+  elGameZone,
+  elResultZone,
+} from "./elements.mjs";
+
+function robotChoose() {
+  const hand = ["rock", "paper", "scissors"];
+  const randomIndex = Math.trunc(Math.random() * hand.length);
+  return hand[randomIndex];
+}
+
+function swape(boolean) {
+  if (boolean) {
+    elGameZone.style.display = "none";
+    elResultZone.style.display = "flex";
+  } else {
+    elGameZone.style.display = "flex";
+    elResultZone.style.display = "none";
+  }
+}
+
+function checkWiner(user, robot) {
+  if (user === robot) {
+    return "TIE";
+  } else if (user === "paper" && robot === "scissors") {
+    return "ROBOT";
+  } else if (user === "scissors" && robot === "rock") {
+    return "ROBOT";
+  } else if (user === "rock" && robot === "paper") {
+    return "USER";
+  }
+}
+
+elHands.forEach((elHands) => {
+  elHands.addEventListener("click", (evt) => {
+    swape(true);
+    const user = evt.target.alt;
+    const robot = robotChoose();
+    elUser.src = evt.target.src;
+    elRobot.src = "./img/hand-load.svg";
+    setTimeout(() => {
+      elRobot.src = `./img/${robot}.svg`;
+      const winner = checkWiner(user, robot);
+      alert(winner);
+    }, 1000);
+  });
+});
+
+elBtn.addEventListener("click", () => {
+  swape(false);
+});
